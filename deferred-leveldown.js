3393c9cefe9dc3778a6f5fd2d9902b145bc2362e
Vincent Weevers <mail@vincentweevers.nl>
2021-04-11 14:01:27 +0200

    Breaking: modernize syntax and bump standard (Level/community#98)

--- deferred-leveldown.js
+++ deferred-leveldown.js
@@ -1,22 +1,25 @@
-var AbstractLevelDOWN = require('abstract-leveldown').AbstractLevelDOWN
-var inherits = require('inherits')
-var DeferredIterator = require('./deferred-iterator')
-var deferrables = 'put get del batch clear'.split(' ')
-var optionalDeferrables = 'approximateSize compactRange'.split(' ')
+'use strict'
+
+const AbstractLevelDOWN = require('abstract-leveldown').AbstractLevelDOWN
+const inherits = require('inherits')
+const DeferredIterator = require('./deferred-iterator')
+const deferrables = 'put get del batch clear'.split(' ')
+const optionalDeferrables = 'approximateSize compactRange'.split(' ')
 
 function DeferredLevelDOWN (db) {
   AbstractLevelDOWN.call(this, db.supports || {})
 
   // TODO (future major): remove this fallback; db must have manifest that
   // declares approximateSize and compactRange in additionalMethods.
-  optionalDeferrables.forEach(function (m) {
+  for (const m of optionalDeferrables) {
     if (typeof db[m] === 'function' && !this.supports.additionalMethods[m]) {
       this.supports.additionalMethods[m] = true
     }
-  }, this)
+  }
 
   this._db = db
   this._operations = []
+
   closed(this)
 }
 
@@ -25,61 +28,61 @@ inherits(DeferredLevelDOWN, AbstractLevelDOWN)
 DeferredLevelDOWN.prototype.type = 'deferred-leveldown'
 
 DeferredLevelDOWN.prototype._open = function (options, callback) {
-  var self = this
-
-  this._db.open(options, function (err) {
+  this._db.open(options, (err) => {
     if (err) return callback(err)
 
-    self._operations.forEach(function (op) {
+    for (const op of this._operations) {
       if (op.iterator) {
-        op.iterator.setDb(self._db)
+        op.iterator.setDb(this._db)
       } else {
-        self._db[op.method].apply(self._db, op.args)
+        this._db[op.method](...op.args)
       }
-    })
-    self._operations = []
+    }
 
-    open(self)
+    this._operations = []
+
+    open(this)
     callback()
   })
 }
 
 DeferredLevelDOWN.prototype._close = function (callback) {
-  var self = this
-
-  this._db.close(function (err) {
+  this._db.close((err) => {
     if (err) return callback(err)
-    closed(self)
+    closed(this)
     callback()
   })
 }
 
 function open (self) {
-  deferrables.concat('iterator').forEach(function (m) {
-    self['_' + m] = function () {
-      return this._db[m].apply(this._db, arguments)
+  for (const m of deferrables.concat('iterator')) {
+    self['_' + m] = function (...args) {
+      return this._db[m](...args)
     }
-  })
-  Object.keys(self.supports.additionalMethods).forEach(function (m) {
-    self[m] = function () {
-      return this._db[m].apply(this._db, arguments)
+  }
+
+  for (const m of Object.keys(self.supports.additionalMethods)) {
+    self[m] = function (...args) {
+      return this._db[m](...args)
     }
-  })
+  }
 }
 
 function closed (self) {
-  deferrables.forEach(function (m) {
-    self['_' + m] = function () {
-      this._operations.push({ method: m, args: arguments })
+  for (const m of deferrables) {
+    self['_' + m] = function (...args) {
+      this._operations.push({ method: m, args })
     }
-  })
-  Object.keys(self.supports.additionalMethods).forEach(function (m) {
-    self[m] = function () {
-      this._operations.push({ method: m, args: arguments })
+  }
+
+  for (const m of Object.keys(self.supports.additionalMethods)) {
+    self[m] = function (...args) {
+      this._operations.push({ method: m, args })
     }
-  })
+  }
+
   self._iterator = function (options) {
-    var it = new DeferredIterator(self, options)
+    const it = new DeferredIterator(self, options)
     this._operations.push({ iterator: it })
     return it
   }
